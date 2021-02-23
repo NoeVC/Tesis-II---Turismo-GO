a14f664b1a6233d8bd51c2202c56d203d0928193
Luis Noe Vicente Canales <51796092+NoeVC@users.noreply.github.com>
2021-02-22 19:58:55 -0500

    Update Gpscontrolador.cs

--- Gpscontrolador.cs
+++ Gpscontrolador.cs
@@ -42,7 +42,7 @@ public class Gpscontrolador : MonoBehaviour
         yield return longitud;
 
         //urlmap = "maps.googleapis.com/maps/api/staticmap?center=" + latitud+","+longitud+"&zoom="+zoom+"&size=600x300"+"&maptype=roadmap&markers=color:blue%7Clabel:S%7C"+latitud+","+longitud+ "&markers = color:red % 7Clabel: S % 7C-15.28209,-73.34621 & key=AIzaSyDKyEc1UikKFmDwgQaICKp-sdYmfYPREVk";
-        string urlmap2 = "https://maps.googleapis.com/maps/api/staticmap?center="+latitud+","+longitud+"&zoom="+zoom+ "&size=400x400&maptype=hybrid&markers=color:red%7Clabel:A%7C" + latitud + "," + longitud + "&markers=color:blue%7Clabel:A%7C-15.279206,-73.344179&markers=color:blue%7Clabel:B%7C-15.278727,-73.344066&markers=color:blue%7Clabel:C%7C-15.278655,-73.344535&markers=color:green%7Clabel:I%7C-15.279123,-73.344599&markers=color:green%7Clabel:I%7C-15.282129,-73.346185&key=AIzaSyDKyEc1UikKFmDwgQaICKp-sdYmfYPREVk";
+        string urlmap2 = "https://maps.googleapis.com/maps/api/staticmap?center="+latitud+","+longitud+"&zoom="+zoom+ "&size=400x400&maptype=hybrid&markers=color:red%7Clabel:A%7C" + latitud + "," + longitud + "&markers=color:blue%7Clabel:A%7C-15.279206,-73.344179&markers=color:blue%7Clabel:B%7C-15.278727,-73.344066&markers=color:blue%7Clabel:C%7C-15.278655,-73.344535&markers=color:green%7Clabel:I%7C-15.279123,-73.344599&markers=color:green%7Clabel:I%7C-15.282129,-73.346185&key=ClaveAPI";
         WWW www = new WWW(urlmap2);
         yield return www;
 
